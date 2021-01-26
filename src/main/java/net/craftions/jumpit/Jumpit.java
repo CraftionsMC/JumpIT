7cf51261cfea672aa0d7f6673c93fc59022fa16c
MCTzOCK <dev@mctzock.de>
2021-01-26 14:58:11 +0100

    First Commit

---
+++ src/main/java/net/craftions/jumpit/Jumpit.java
@@ -0,0 +1,17 @@
+package net.craftions.jumpit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Jumpit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
